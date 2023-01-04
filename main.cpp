ff519af594901d2ca9ae46cfb2781374bd17dad1
Dize <dengze4932@gmail.com>
2023-01-05 00:09:48 +0800

    完成hw03

--- main.cpp
+++ main.cpp
@@ -20,37 +20,37 @@ template <class T1, class T2>
 decltype(auto) operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
-    std::vector<decltype(T1(1)+T2(1))> vec;
-    vec.resize(std::max(a.size(),b.size()));
+    std::vector<decltype(T1{}+T2{})> vec;
+    vec.resize(std::min(a.size(),b.size()));
     for(int i = 0; i < vec.size(); ++i)
     {
-        if (i < a.size() && i < b.size()){
-            vec[i] = a[i]+b[i];    
-        }
-        else{
-            if (i < a.size()){
-                vec[i] = a[i];
-            }
-        else if (i < b.size()){
-            vec[i] = b[i];
-        }
+        vec[i] = a[i] + b[i];    
     }
-}
+
     return vec;
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
 // 请实现自动匹配容器中具体类型的加法！10 分
-    return 
+    return
     std::visit([&](const auto& t1, const auto& t2) -> std::variant<T1,T2>{
         return t1 + t2;
     },a,b);
-   
+
+}
+
+template <class T1, class T2, class T3>
+std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, T3 const &b) {
+    static_assert(std::is_same_v<T1,T3> || std::is_same_v<T2,T3>);
+    return std::visit([&](const auto& v)->std::variant<T1, T2>{
+        return v + b;
+    },a);
+
 }
 
 template <class T1, class T2>
-std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
+std::ostream& operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
     return 
     std::visit([&](const auto& t) -> std::ostream&{
@@ -75,7 +75,7 @@ int main() {
     std::variant<std::vector<int>, std::vector<double>> d = c;
     std::variant<std::vector<int>, std::vector<double>> e = a;
     d = d + c + e;
-
+    //d = d + e;
     // 应该输出 {9.28, 17.436, 7.236}
     std::cout << d << std::endl;
 
